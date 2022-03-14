3a1c67cdbb6597f33ff580b31bde3af6d64a0123
996Worker <69796042+Blackmesa-Canteen@users.noreply.github.com>
2022-03-15 00:53:55 +1100

    fix: can render now. feature: list

--- src/component/Expense/ExpenseItem.js
+++ src/component/Expense/ExpenseItem.js
@@ -1,30 +1,21 @@
-import ExpenseDate from "./ExpenseDate";
-import "./ExpenseItem.css"
-import Card from "../UI/Card";
-import {useState} from "react";
+import React from 'react';
 
-function ExpenseItem(props) {
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
 
-    const [title, setTitle] = useState(props.title);
-
-    const clickHandler = () => {
-        setTitle("Updated");
-        console.log(title);
-    };
-
+const ExpenseItem = (props) => {
     return (
-        <Card className="expense-item">
-
-            <ExpenseDate date={props.date}/>
-            <div className="expense-item__description">
-                <h2 className="expense-item__description">  {title}</h2>
-                <div className="expense-item__price"><h3>{props.amount}</h3></div>
-            </div>
-
-            <button onClick={clickHandler}>Change Title</button>
-        </Card>
+        <li>
+            <Card className='expense-item'>
+                <ExpenseDate date={props.date} />
+                <div className='expense-item__description'>
+                    <h2>{props.title}</h2>
+                    <div className='expense-item__price'>${props.amount}</div>
+                </div>
+            </Card>
+        </li>
     );
-}
+};
 
 export default ExpenseItem;
-
